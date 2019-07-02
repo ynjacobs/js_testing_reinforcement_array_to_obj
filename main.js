634900d8161a118a20350418ced62c48d4b608fe
ynjacobs <ytj64611@gamil.com>
2019-07-02 12:08:55 -0400

    test past for changing nested array to object

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(myArray) {
+    let objToReturn = {};
+    myArray.forEach(e => objToReturn[e[0]] = e[1]); 
+    return objToReturn;
 }
 
 module.exports = arrayToObj;
--- main.test.js
+++ main.test.js
@@ -1,6 +1,13 @@
 const arrayToObj = require('./main.js');
 
 test('arrayToObj converts nested array to JS object', () => {
-  const result = arrayToObj([['Alfonso Cuaron', 'Gravity'], ['Spike Jonze', 'Her'], ['Martin Scorsese', 'The Wolf of Wall Street']]);
-  expect(result).toEqual({"Alfonso Cuaron": "Gravity", "Spike Jonze": "Her", "Martin Scorsese": "The Wolf of Wall Street"});
+  const result = arrayToObj(
+    [
+      ['Alfonso Cuaron', 'Gravity'], 
+      ['Spike Jonze', 'Her'], 
+      ['Martin Scorsese', 'The Wolf of Wall Street']
+    ]
+    );
+  expect(result).toEqual(
+    {"Alfonso Cuaron": "Gravity", "Spike Jonze": "Her", "Martin Scorsese": "The Wolf of Wall Street"});
 });
